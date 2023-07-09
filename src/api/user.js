35bd3fcfa083aedd4e0f6e61fd6f71c967d3b138
zjm <2215539769@qq.com>
2023-07-09 09:58:25 +0800

    完成初始化人力资源管理项目

---
+++ src/api/user.js
@@ -0,0 +1,12 @@
+// import request from '@/utils/request'
+
+export function login(data) {
+
+}
+
+export function getInfo(token) {
+
+}
+
+export function logout() {
+}
---
+++ src/icons/index.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import SvgIcon from '@/components/SvgIcon'// svg component
+
+// register globally
+Vue.component('svg-icon', SvgIcon)
+
+const req = require.context('./svg', false, /\.svg$/)
+const requireAll = requireContext => requireContext.keys().map(requireContext)
+requireAll(req)
+// 代码的意思就是将所有的svg目录下的.svg后缀全都引入到项目中
