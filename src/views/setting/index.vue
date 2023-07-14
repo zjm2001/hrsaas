a53b0f3881fde20b8c8d9e10f3d7d8735bffc72c
zjm <2215539769@qq.com>
2023-07-14 10:40:30 +0800

    公司设置基本结构完成(写了删除员工分页展示数据)

--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -1,19 +1,115 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        公司设置
-      </h2>
+      <el-card>
+        <el-tabs v-model="activeName">
+          <el-tab-pane label="角色管理" name="first">
+            <el-row style="height: 60px">
+              <el-col> <el-button type="primary" icon="el-icon-plus">新增角色</el-button></el-col>
+            </el-row>
+            <!-- 表格 -->
+            <el-table border :data="list">
+              <el-table-column align="center" label="序号" width="120" type="index" />
+              <el-table-column align="center" prop="name" label="名称" width="240" />
+              <el-table-column align="center" prop="description" label="描述" />
+              <el-table-column align="center" label="操作">
+                <template slot-scope="{ row }">
+                  <el-button size="small" type="success" icon="el-icon-s-check">分配权限</el-button>
+                  <el-button size="small" type="primary" icon="el-icon-edit">编辑</el-button>
+                  <el-button size="small" type="danger" icon="el-icon-delete" @click="deleteRole(row.id)">删除</el-button>
+                </template>
+
+              </el-table-column>
+            </el-table>
+            <!-- 分页组件 -->
+            <el-row type="flex" justify="center" align="middle" style="height: 60px">
+              <el-pagination
+                :current-page="page.page"
+                :page-size="page.pagesize"
+                :total="page.total"
+                layout="prev,pager,next"
+                @current-change="changePage"
+              />
+            </el-row>
+          </el-tab-pane>
+          <el-tab-pane label="公司信息" name="second">
+            <el-alert title="对公司名称、公司地址、营业执照、公司地区的更新，将使得公司资料被重新审核，请谨慎修改" type="info" show-icon :closable="false" />
+            <el-form label-width="120px" style="margin-top:50px">
+              <el-form-item label="公司名称">
+                <el-input v-model="formData.name" disabled style="width:400px" />
+              </el-form-item>
+              <el-form-item label="公司地址">
+                <el-input v-model="formData.companyAddress" disabled style="width:400px" />
+              </el-form-item>
+              <el-form-item label="邮箱">
+                <el-input v-model="formData.mailbox" disabled style="width:400px" />
+              </el-form-item>
+              <el-form-item label="备注">
+                <el-input v-model="formData.remarks" type="textarea" :rows="3" disabled style="width:400px" />
+              </el-form-item>
+            </el-form>
+          </el-tab-pane>
+        </el-tabs>
+      </el-card>
     </div>
   </div>
 </template>
 
 <script>
+import { getRoleList, getCompanyInfo, deleteRole } from '@/api/setting'
 export default {
+  data() {
+    return {
+      activeName: 'second',
+      list: [], // 接收表单数据
+      page: {
+        // 放置页码及相关数据
+        page: 1,
+        pagesize: 8,
+        total: 0 // 记录总数
+      },
+      formData: {}
+    }
+  },
+  created() {
+    this.getRoleList()
+    this.getCompanyInfo()
+  },
+  methods: {
+    /** 获取列表数据 */
+    async getRoleList() {
+      const { total, rows } = await getRoleList(this.page)
+      this.page.total = total
+      this.list = rows
+    },
+    /** 分页组件的事件 */
+    changePage(newPage) {
+      // newPage是当前点击的页码
+      this.page.page = newPage // 将当前页码赋值给当前的最新页码
+      this.getRoleList()
+    },
+    /** 获取的公司的信息 */
+    async getCompanyInfo() {
+      this.formData = await getCompanyInfo(this.companyId)
+    },
+    /** 点击删除 */
+    async  deleteRole(id) {
+      this.$confirm('确定要删除该角色吗', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        // 如果点击了确定就会进入then
+        return deleteRole(id) // 返回promise对象
+      }).then(() => {
+        //  如果删除成功了  就会进入这里
+        this.getRoleList()
+        this.$message.success('删除角色成功')
+      })
+    }
+  }
 
 }
 </script>
 
-  <style>
-
-  </style>
+<style></style>
